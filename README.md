21532ef08ef28e627cd96571cdc7b7cf710dd3d5
yoshinaga_masahiro <anyapp.yoshinaga.mahiro@gmail.com>
2025-02-15 16:22:42 +0900

    メールBE接続

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-server2
+
+#サーバー接続コマンド
+node src/index.js
+
+#メールサーバー起動
+node src/server.js
---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// .envの読み込み
+dotenv.config();
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.RECEIVER_EMAIL,
+    // 件名↓
+    subject: `${name}`,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.json({ success: true, message: "メールが送信されました。" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
