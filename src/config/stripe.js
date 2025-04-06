82fab65d1758cc18930d9b1e3cb82a5ef2fccde4
yoshinagamasahiro1197-ym <yoshinaga@vinayamato.com>
2025-04-06 16:59:24 +0900

    ecサイト: 500エラーの修正

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,10 +1,6 @@
 // @ts-ignore
-const stripe = require("stripe");
-
-// @ts-ignore
-const stripeClient = stripe(
-  process.env.STRIPE_SECRET_KEY ||
-    "sk_test_51RAnCtJITRXES6s8tTBIWEgmliISWJSFlfDN8tL6xNYbrM4GHTTebdF5wXGDCuRAqTUTgFYx4QR2AUn0Jim45cVY00UPZhebzF"
+const stripe = require("stripe")(
+  "sk_test_51RAnCtJITRXES6s8tTBIWEgmliISWJSFlfDN8tL6xNYbrM4GHTTebdF5wXGDCuRAqTUTgFYx4QR2AUn0Jim45cVY00UPZhebzF"
 );
 
-module.exports = stripeClient;
+module.exports = stripe;
